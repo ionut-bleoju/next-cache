eb434427e3244120c56deeaf6066dc0e0dcacf16
Bleoju Ionut Bogdan <ionutyop@gmail.com>
2025-10-10 13:06:30 +0300

    Removed static generation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,11 @@ import NextIcon from "public/next.svg";
 import GlobeIcon from "public/globe.svg";
 import FileIcon from "public/file.svg";
 import WindowIcon from "public/window.svg";
-
-export const revalidate = 3600;
+import { cookies } from "next/headers";
 
 export default async function Home() {
+  const test = await cookies();
+  console.log(test.getAll());
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
