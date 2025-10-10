3aa9405bf7eff851156104243e741dd650027bee
Bleoju Ionut Bogdan <ionutyop@gmail.com>
2025-10-10 14:36:24 +0300

    Removed async

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import GlobeIcon from "public/globe.svg";
 import FileIcon from "public/file.svg";
 import WindowIcon from "public/window.svg";
 
-export default async function Home() {
+export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
